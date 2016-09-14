1d0047753b2db8b3ed8136101e7eb7f823e5180b
Your Name <you@exemple.com>
2016-09-14 07:02:29 -0300

    ultimas alterações

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -4,6 +4,7 @@ import pickle
 from pprint import pprint
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import pairwise_distances
+import matplotlib.pyplot as plt
 
 def ranking(pairwise_results):
     
@@ -12,8 +13,8 @@ def ranking(pairwise_results):
     
     for j in range(len(pairwise_results)):
 
-        indices.append([i[0] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1])])
-        distancias.append([i[1] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1])])
+        indices.append([i[0] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1], reverse=True)])
+        distancias.append([i[1] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1], reverse=True)])
     	
     return indices, distancias
 
@@ -27,11 +28,13 @@ def f1_score(precision, recall):
 
 
 def metricas(targets, indices, distancias):
+    
     indices = np.array(indices)
     distancias = np.array(distancias)
 
     precisions = np.zeros(indices.shape[1])
     recalls = np.zeros(indices.shape[1])
+    selectivitys = np.zeros(indices.shape[1])
     f1 = np.zeros(indices.shape[1])
     hfm = np.zeros(len(targets))
     separation = np.zeros(len(targets))
@@ -48,37 +51,44 @@ def metricas(targets, indices, distancias):
         
         for j in range(len(rankingi)):
             
-            retornados = j+1
+            retornados +=1
         	
             if targeti[rankingi[j]] == 1:
                 relevantes += 1
-                separation[i] = (distancias[i][j] * 100. / distancias[i][0]) - hfm[i]
+                #separation[i] = (distancias[i][j] * 100. / distancias[i][0]) - hfm[i]
 
-            elif hfm[i] == 0:
-                hfm[i] =  distancias[i][j] * 100. / distancias[i][0]
+            #elif hfm[i] == 0:
+                #hfm[i] =  distancias[i][j] * 100. / distancias[i][0]
             
             precision = relevantes/retornados
             recall = relevantes/total_relevantes
+            selectivity = retornados/indices.shape[1]
 
             precisions[j] = precisions[j] + precision
             recalls[j] = recalls[j] + recall
-
+            selectivitys[j] = selectivitys[j] + selectivity
             f1[j] = f1[j] + f1_score(precision, recall)
 
+
+    selectivitys /= len(targets)
     precisions /= len(targets)
-    recalls /= len(targets)  
+    recalls /= len(targets)
     f1 /= len(targets)
+    return precisions, recalls, f1, hfm, separation, selectivitys
 
-   
-    return precisions, recalls, f1, hfm, separation
+'''
+all_texts = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/at.txt", "rb"))
+ft = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/ft.txt", "rb"))
+targets = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/targets.txt", "rb"))
+tam_queries = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/tam_queries.txt", "rb"))
+tam_corpus = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/tam_corpus.txt", "rb"))
+'''
 
-
-all_texts = pickle.load(open("/home/jones/resultados/at.txt", "rb"))
-ft = pickle.load(open("/home/jones/resultados/ft.txt", "rb"))
-targets = pickle.load(open("/home/jones/resultados/targets.txt", "rb"))
-tam_queries = pickle.load(open("/home/jones/resultados/tam_queries.txt", "rb"))
-tam_corpus = pickle.load(open("/home/jones/resultados/tam_corpus.txt", "rb"))
-
+all_texts = pickle.load(open("/home/forrest/Documentos/resultados/hu0_n100/at.txt", "rb"))
+ft = pickle.load(open("/home/forrest/Documentos/resultados/hu0_n200/ft.txt", "rb"))
+targets = pickle.load(open("/home/forrest/Documentos/resultados/hu0_n100/targets.txt", "rb"))
+tam_queries = pickle.load(open("/home/forrest/Documentos/resultados/hu0_n100/tam_queries.txt", "rb"))
+tam_corpus = pickle.load(open("/home/forrest/Documentos/resultados/hu0_n100/tam_corpus.txt", "rb"))
 sentences_docs = []
 sentences_queries = []
 count = 0
@@ -99,25 +109,54 @@ for i in range(0, len(ft), 2*tam_corpus):
 	l = []
 	j = []
 	for sentence in ft[i]:
+
 		j.append(np.sum(sentence[2], axis=0))#conjunto de todos os vetores de todos os nós de todas as sentencas
 		l.append(sentence[2][0])
+			
 	sentences_queries.append(np.sum(j, axis=0))
 	root_sentences_queries.append(l)#conjunto de todos os vetores raizes de todas as sentencas
 
 
-docs_results = [np.inf]*tam_queries*tam_corpus
+docs_results = [0.0]*tam_queries*tam_corpus
 docs_results = np.array(docs_results).reshape(tam_queries, tam_corpus)
 
+
+nbrs = NearestNeighbors(n_neighbors=2, algorithm='kd_tree').fit(root_sentences_docs)
+
 for i, sentences in enumerate(root_sentences_queries):
-	nbrs = NearestNeighbors(n_neighbors=5, algorithm='ball_tree').fit(root_sentences_docs)
+
+	
 	distances, indices = nbrs.kneighbors(sentences)
 	suspects = []
 	sources = []
-	for posicao in indices:
-		suspects.append(mapa[ posicao[0] ])
-	
+	for queries_i in indices:
+		#suspects.append(mapa[ posicao[0] ])
+		for k in queries_i:
+			docs_results[i][mapa[k]] += 1
+	'''
 	for doc in set(suspects):
-		docs_results[i][doc] = pairwise_distances(sentences_queries[i].reshape(1,-1), sentences_docs[doc].reshape(1,-1), 'euclidean')[0][0]
+		#docs_results = np.array(len(root_sentences_docs[doc]), len(sentences))
+		t = []
+		for key, value in mapa.items():
+			if value == doc:
+				t.append(root_sentences_docs[key])
+		s = 0
+		for iter_doc, k in enumerate(t):
+			
+			for iter_querie, l in enumerate(sentences):
+				#print(pairwise_distances(k.reshape(1,-1), l.reshape(1,-1), 'euclidean'))
+				d = pairwise_distances(k.reshape(1,-1), l.reshape(1,-1), 'euclidean')
+				if d > 1.2:
+					s += d
 
+
+		docs_results[i][doc] = s
+	'''
+#print(docs_results)
 indices, distances = ranking(docs_results)
-precision, recall, f1, hfm, separation = metricas(targets, indices, distances)
+precision, recall, f1, hfm, separation, selectivity = metricas(targets, indices, distances)
+print(recall)
+print(selectivity)
+
+plt.plot(recall, selectivity, marker='.')
+plt.show()
