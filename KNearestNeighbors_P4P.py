4c0e956a556d1f192927e4c192d348706394ff99
jonesmarxs <jonesmarxs@gmail.com>
2016-11-27 23:47:50 -0200

    correção do codigo

--- KNearestNeighbors_P4P.py
+++ KNearestNeighbors_P4P.py
@@ -14,7 +14,7 @@ def ranking(pairwise_results):
 
         indices.append([i[0] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1], reverse=True)])
         distance.append([i[1] for i in sorted(enumerate(pairwise_results[j]), key=lambda x:x[1], reverse=True)])
-        
+    
     return indices, distance
 
 def metricas(targets, indices):
@@ -30,10 +30,10 @@ def metricas(targets, indices):
     for i in range(len(targets_dense)):
         relevantes = 0
 
-        rankingi = indices[2]
+        rankingi = indices[i]
 
         #targeti = targets[i]
-        targeti = np.transpose(targets_dense[2])
+        targeti = np.transpose(targets_dense[i])
         total_relevantes = np.sum(targeti)
 
         for j in rankingi:
@@ -46,7 +46,7 @@ def metricas(targets, indices):
             selectivitys[j] += (j+1) / indices.shape[1]
         
     selectivitys /= len(targets_dense)
-    
+    print(recalls)
     recall = np.mean(recalls[:, [0, 50, 100, 213, 427, 641, 855]], axis=0)
     
     selectivitys = [0, 50, 100, 213, 427, 641, 855]
@@ -60,11 +60,11 @@ tam_queries = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/res
 tam_corpus = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/tam_corpus.txt", "rb"))
 '''
 
-all_texts = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/pan_2011/at_200.txt", "rb"))
-ft = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/pan_2011/ft_200.txt", "rb"))
-targets = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/pan_2011/targets_200.txt", "rb"))
-tam_queries = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/pan_2011/tam_queries_200.txt", "rb"))
-tam_corpus = pickle.load(open("/home/forrest/workspace/BRI/Final Work/final/resultados/pan_2011/tam_corpus_200.txt", "rb"))
+all_texts = pickle.load(open("/home/jones/Documentos/BRI/dataset/P4P/at_200.txt", "rb"))
+ft = pickle.load(open("/home/jones/Documentos/BRI/dataset/P4P/ft_200.txt", "rb"))
+targets = pickle.load(open("//home/jones/Documentos/BRI/dataset/P4P/targets_200.txt", "rb"))
+tam_queries = pickle.load(open("/home/jones/Documentos/BRI/dataset/P4P/tam_queries_200.txt", "rb"))
+tam_corpus = pickle.load(open("/home/jones/Documentos/BRI/dataset/P4P/tam_corpus_200.txt", "rb"))
 
 sentences_docs = []
 sentences_queries = []
@@ -98,16 +98,20 @@ for i in range(0, 2*tam_queries, 2):
     root_sentences_queries.append(l)#conjunto de todos os vetores raizes de todas as sentencas
 
 
-docs_results = [0.0]*tam_queries*tam_corpus
+docs_results = [0.0]*tam_corpus*tam_queries
 docs_results = np.array(docs_results).reshape(tam_queries, tam_corpus)
+print(docs_results.shape)
+k = 50
+'''
+brute = NearestNeighbors(n_neighbors=50, algorithm='brute', metric='euclidean').fit(root_sentences_docs)
 
-nbrs = NearestNeighbors(n_neighbors=50, algorithm='brute', metric='euclidean').fit(root_sentences_docs)
-k = 5
+y_brute = []
+
 for i in queries_indices:
     
     sentences = root_sentences_queries[i]
 
-    distances, indices = nbrs.kneighbors(sentences)
+    distances, indices = brute.kneighbors(sentences)
 
     a = set()
     for j, queries_i in enumerate(indices):
@@ -116,16 +120,65 @@ for i in queries_indices:
             if len(a) > k:
                 break
             docs_results[i][mapa[m]] += 1
+            y_brute.append(distances[j, l])
             
         if len(a) > k:
             break
 
-#targets_dense = targets.todense()
-#pprint(docs_results[10])
-#pprint(np.nonzero(docs_results[657]))
+kdTree = NearestNeighbors(n_neighbors=50, algorithm='kd_tree', metric='euclidean').fit(root_sentences_docs)
+
+y_kd = []
+
+for i in queries_indices:
+    
+    sentences = root_sentences_queries[i]
+
+    distances, indices = kdTree.kneighbors(sentences)
+
+    a = set()
+    for j, queries_i in enumerate(indices):
+        for l, m in enumerate(queries_i):
+            a.add(mapa[m])
+            if len(a) > k:
+                break
+            docs_results[i][mapa[m]] += 1
+            y_kd.append(distances[j, l])
+            
+        if len(a) > k:
+            break
+'''
+ballTree = NearestNeighbors(n_neighbors=50, algorithm='ball_tree', metric='euclidean').fit(root_sentences_docs)
+
+y_ball = []
+
+for i in queries_indices:
+    
+    sentences = root_sentences_queries[i]
+
+    distances, indices = ballTree.kneighbors(sentences)
+
+    a = set()
+    for j, queries_i in enumerate(indices):
+        for l, m in enumerate(queries_i):
+            a.add(mapa[m])
+            if len(a) > k:
+                break
+            docs_results[i][mapa[m]] += 1
+            y_ball.append(distances[j, l])
+            
+        if len(a) > k:
+            break
+
+docs_results = docs_results[queries_indices, :]
+print(docs_results.shape)
+'''
+x = np.zeros(len(y_brute))
+plt.plot(np.mean(x), np.mean(y_brute), 'ro', np.mean(x+1), np.mean(y_kd), 'bo', np.mean(x+2), np.mean(y_ball), 'go')
+plt.show()
+'''
 
 indices, distances = ranking(docs_results)
 recall, selectivity = metricas(targets, indices)
 
-plt.plot(selectivity, recall, marker='.')
+plt.plot(selectivity, recall, marker='o')
 plt.show()
